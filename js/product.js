cb897149f7e99ff313723edb844ac5857332f33c
lehuuduc104 <2051060449@e.tlu.edu.vn>
2022-03-31 13:57:37 +0700

    up product

---
+++ js/product.js
@@ -0,0 +1,8 @@
+function price40(){
+    let price_re=document.getElementById('price');
+    price_re.textContent='$270'
+}
+function price44(){
+    let price_re=document.getElementById('price');
+    price_re.textContent='$350'
+}
